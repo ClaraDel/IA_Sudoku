7bf6883120704621a638c3a234f98d23d19acd13
ClaraDel <clara.ferru@epfedu.fr>
2021-10-21 10:04:56 -0400

    création grille

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 
+class Grille :
+    
+    def __init__(self):
+        # Création des 16 cases
+        self.caseList = []
+
+        for i in range(4):
+            for j in range(4):
+                self.caseList.append((i,j))
