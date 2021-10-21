b489abd12821afa084b14b0ed88425db5fede4bc
ClaraDel <clara.ferru@epfedu.fr>
2021-10-21 10:07:45 -0400

    mini commit

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ class Grille :
     
     def __init__(self):
         # Création des 16 cases
-        self.caseList = []
+        self.grille = []
 
         for i in range(4):
             for j in range(4):
-                self.caseList.append((i,j))
+                self.grille.append((0,0))
+                
+        
