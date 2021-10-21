4b0185a048a4f4a86d1e83f0c16fe788b0b8ecf1
alexis <alexis.shan-yan@insa-lyon.fr>
2021-10-21 12:21:04 -0400

    added check consistency

--- Model/Grille.py
+++ Model/Grille.py
@@ -34,20 +34,19 @@ class Grille :
 
         for currentDomainValue in len(self.domain):
 
-            # --------------------
             # If The value we want to assign is consistent with indexChosen, continue the program
             # else, wait for another iteration of the for loop
-            # --------------------
+            if (self.checkConsistency(indexChosen, currentDomainValue)):
 
-            self.grille[indexChosen] = currentDomainValue
+                self.grille[indexChosen] = currentDomainValue
 
-            result = self.backTracking(self)
+                result = self.backTracking(self)
 
-            if (result != False):
-                return result
+                if (result != False):
+                    return result
 
-            # If it leads nowhere (failure), we put the value of the chosen index back to 0
-            self.grille[indexChosen] = 0
+                # If it leads nowhere (failure), we put the value of the chosen index back to 0
+                self.grille[indexChosen] = 0
 
         return False
 
@@ -68,6 +67,21 @@ class Grille :
                 sudokuCompleted = False
 
         return sudokuCompleted
+
+
+
+    def checkConsistency(self, currentCase, valueToTest):
+        consistencyOk = True
+        casesWithConstraint = self.getCaseConstraint(self.getIndice(currentCase))
+        for case in casesWithConstraint:
+            if (self.grille[case] == valueToTest):
+                consistencyOk = False
+        return consistencyOk
+
+
+
+
+
         
     def degreeHeuristic(self) :
         selectedCases = []
