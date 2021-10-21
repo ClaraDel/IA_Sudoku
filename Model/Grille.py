00cc2626a72af904cda3732daeb8329325159f8b
alexis <alexis.shan-yan@insa-lyon.fr>
2021-10-21 11:21:29 -0400

    added some comments

--- Model/Grille.py
+++ Model/Grille.py
@@ -10,25 +10,44 @@ class Grille :
             for j in range(self.taille):
                 self.grille.append(0)
 
+
+
+    # get the case of the grid with the coordinates "i" and "j"
     def get_case(self,i, j):
         return i*self.taille + j
 
+
+
+    # the main function of the program
     def backTracking(self, currentIndexChosen, currentDomainValue):
 
+        # We assign the domainValue chosen by the previous "iteration" of backTracking to the chosen index
         self.grille[currentIndexChosen] = currentDomainValue
 
+        # We check the completion of the sudoku
         if (self.checkCompletion()):
             print("You have won !")
 
+        # We get the list of index to explore and check if the value is equal to 0 (value not yet assigned)
         for index in len(self.chooseIndexList()):
             if (self.grille[index] == 0):
-                for domainValue in self.domain:
+
+                # Check if the domain value is possible for this index, if not, check for another value
+                for domainValue in len(self.domain):
                     self.backTracking(self, index, self.domain[domainValue])
 
+                    # If no value was found for this index, do : grille[currentIndexChosen] = 0
+
+
+
+    # We create a list of index to explore, sorted according to the heuristics
     def chooseIndexList(self):
         # Put here the heuristics
         return self.grille
 
+        
+
+    # Checking the completion of the sudoku
     def checkCompletion(self):
         sudokuCompleted = True
         for i in self.grille:
