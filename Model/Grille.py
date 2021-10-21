b6e3aef8ef1b100324bc0d95dcdae7d50b19bc8b
ClaraDel <clara.ferru@epfedu.fr>
2021-10-21 11:40:01 -0400

    Ajout degreeHeuristic, getDomainPossible et MRV

--- Model/Grille.py
+++ Model/Grille.py
@@ -1,8 +1,11 @@
+import copy
+
 class Grille :
 
     def __init__(self):
         # Création des 16 cases
         self.taille = 4
+        self.length = self.taille*self.taille
         self.grille = []
         self.domain = list(range(1,self.taille+1))
 
@@ -31,8 +34,53 @@ class Grille :
 
     def checkCompletion(self):
         sudokuCompleted = True
-        for i in self.grille:
+        for i in self.length:
             if (self.grille[i] == 0):
                 sudokuCompleted = False
 
         return sudokuCompleted
+        
+    def degreeHeuristic(self) :
+        selectedCases = []
+        for i in self.length:
+            if (self.grille[i] == 0): 
+                selectedCases.add(i)
+        maxSum = 0
+        returnValues = []
+        
+        for case in selectedCases:
+            neighboursCase = fonctionAlex(case)
+            sumNeighboursNull = 0
+            
+            for neighbours in neighboursCase :
+                if self.grille[neighbours] == 0:
+                    sumNeighboursNull += 1
+            if sumNeighboursNull == maxSum:
+                returnValues.add(case)
+            if(sumNeighboursNull > maxSum):
+                maxSum = sumNeighboursNull
+                returnValues = []
+                returnValues.add(case)
+        return returnValues
+                    
+    def MRV(self) :
+        minTaille = self.taille
+        returnValues = []
+        for i in self.length:
+            if (self.grille[i] == 0): 
+                domainPossible = self.getDomainPossible(i)
+                if len(domainPossible) == minTaille:
+                    returnValues.add(i)
+                if len(domainPossible) < minTaille:
+                    minTaille = len(domainPossible)
+                    returnValues = []
+                    returnValues.add(i)
+        return returnValues
+                    
+                
+    def getDomainPossible(self, index):
+        domainPossible = copy.deepcopy(self.domain)
+        neighboursCase = fonctionAlex(index) ;
+        for i in neighboursCase:
+            domainPossible.remove(self.grille[i])
+        return domainPossible
